1f86bc0bd32695f0193b0199a240cd37b8aeb420
RohanBh <brohan52@gmail.com>
2018-12-16 22:24:29 +0530

    Add parser

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from scipy import stats
 from scipy import special
 from numpy import np
+import pandas as pd
 
 def multibeta(alpha, axis = 0, q = -1):
     if q == -1:
@@ -37,21 +38,42 @@ def posterior(x, model, alpha, q, k, l):
     b = model_prior(k, l)
     return a.pdf(x) * b(model)
 
-def metropolis(x, s, it = 1000, alpha = 0.1, q, k, l):
-    """s is the number of predictors, x is the 2D array,
+def metropolis(x, arity, it = 1000, alpha = 0.1, q, k=np.e, l=4):
+    """arity is the number of predictors, x is the 2D array,
     alpha is the param for theta's prior, q is the number of 
     possible o/ps for bool fn, k is the strength param for 
     model prior and l is the cutoff (sort of) for same"""
     sample = []
-    model = np.random.randint(0, 2, s)    
+    model = np.random.randint(0, 2, arity)    
     sample.add(model)
 
-    for i in xrange(it):
+    for i in range(it):
         next_model = np.array(model)
-        next_model[np.random.randint(0, s)] ^= 1
+        next_model[np.random.randint(0, arity)] ^= 1
         a = posterior(x, next_model, alpha, q, k, l) / posterior(x, model, alpha, q, k, l)
         if np.random.rand() <= a:
             model = next_model
         sample.add(model)
 
+    return sample
 
+def parse(X, Y, arity, q):
+    xi = np.core.defchararray.add('x', np.char.mod('%d', np.arange(X.shape[1])))
+    cols = np.append(xi, 'y')
+
+    X = np.hstack((X, Y[:, np.newaxis]))
+    
+    df = pd.DataFrame(X, columns=temp)
+    df = df.assign(count=-1).groupby(cols).count()
+
+    allY = pd.DataFrame(np.arange(q), columns=['y'])
+
+    x = []
+    for name, group in df.groupby(xi):
+        vx = group.loc[:, ['y', 'count']]
+        vx = pd.merge(vx, allY, how='right', on='y')
+        vx.fillna(0, inplace=True) # replaces NaN with 0
+        vx = vx.sort_values(by=['y']).values
+        x.append(x, vx)
+
+    return np.array(x)
