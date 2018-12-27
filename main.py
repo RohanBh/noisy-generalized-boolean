274224041249c43c5840fbcb27e02c08caf93693
RohanBh <brohan52@gmail.com>
2018-12-27 14:38:03 +0530

    Restructure the repo

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-from scipy import stats
 from scipy import special
 import numpy as np
 import pandas as pd
-from mpmath import *
 import time
-
-mp.pretty = True
 
 def pdfYgivenXm(Xm, alpha, q, Y):
     """Y is the response of system, Xm is the
@@ -16,47 +12,28 @@ def pdfYgivenXm(Xm, alpha, q, Y):
     P(Y | Xm) = product--------------------------------------------------------------------
                                       (gamma(alpha) ** q) * gamma(nx + q*alpha)
     """
-    start = time.time()
     # x is the 2D array like [vx1,vx2, ..., vxk] where 
     # vx is the 1D array of q elements
     x = parse(Xm, Y, q)
-    x = matrix(x.tolist())
-    alpha = mpf(alpha)
-    q = mpf(q)
     x = x + alpha
-    a = zeros(x.rows, x.cols)
-    for i in range(x.rows):
-        for j in range(x.cols):
-            a[i, j] = gamma(x[i, j]) / gamma(alpha)
-    a1 = ones(a.rows, 1)
-    for i in range(a.rows):
-        for j in range(a.cols):
-            a1[i, 0] *= a[i, j]
-    b = zeros(x.rows, 1)
-    x = x * ones(x.cols, 1)
-    for i in range(x.rows):
-        b[i, 0] = gamma(q * alpha) / gamma(x[i, 0])
-    # a = np.product(multibeta(x + alpha, axis=1), dtype=float)
-    # b = np.power(multibeta(alpha, q=q), x.shape[0], dtype=float)
-    p = mpf(1)
-    for i in range(a.rows):
-        p *= a1[i, 0] * b[i, 0]
-
-    end = time.time()
-    print(f"pdf computation took {end - start} seconds")
-    return p
+    num = special.gamma(x)
+    den = special.gamma(x.sum(axis=1))
+    num = np.log(num).sum(axis=1)
+    den = np.log(den)
+    num -= den
+    num += np.log(special.gamma(q * alpha)) - q * np.log(special.gamma(alpha))
+    return num.sum()
 
 def model_prior(model, k=np.e, l=4):
     """model is represented by a numpy boolean array"""
     model_size = np.count_nonzero(model)
-    p = mpf(min(0, l - model_size))
-    return k ** p
+    p = min(0, l - model_size)
+    return p * np.log(k)
 
 def posterior(Y, X, model, alpha, q, k, l):
     Xm = X[:, model]
-    a, b = pdfYgivenXm(Xm, alpha, q, Y), model_prior(model, mpf(k), mpf(l))
-    print(f"P(Y|Xm) = {a}    P(model) = {b}")
-    return a * b
+    a, b = pdfYgivenXm(Xm, alpha, q, Y), model_prior(model, k, l)
+    return a + b
 
 def metropolis(X, Y, arity, q, it = 500, alpha = 0.1, k=np.e, l=4):
     """arity is the number of predictors, alpha is the param 
@@ -65,22 +42,29 @@ def metropolis(X, Y, arity, q, it = 500, alpha = 0.1, k=np.e, l=4):
     is the cutoff (sort of) for same"""
     sample = []
     model = np.round(np.random.randint(0, 2, arity)) != 0
-    print(np.count_nonzero(model))
     sample.append(model)
 
+    t = time.time()
+    lt = time.time()
     for i in range(it):
+        if i % 500 == 0:
+            print(i, f"took {time.time()-lt} seconds")
+            lt = time.time()
+        start = time.time()
         next_model = model.copy()
         next_model[np.random.randint(0, arity)] ^= True
         n, d = posterior(Y, X, next_model, alpha, q, k, l), posterior(Y, X, model, alpha, q, k, l)
-        a = n / d
-        print(f"{i:5}  size={np.count_nonzero(model):2}     next={n}     curr={d}     a={a}", end=' ')
-        if np.random.rand() <= a:
+        a = n - d
+        # print(f"{i:5}  size={np.count_nonzero(model):2}     next_log={n:.2f}     curr_log={d:.2f}     a={(np.e ** a):.2f}", end=' ')
+        if np.log(np.random.rand()) <= a:
             model = next_model
-            print("*"*20, end = " ")
-        print()
-        print("-"*50)
+            # print("*"*5, "change model", "*"*5, end = " ")
+        end = time.time()
+        # print()
+        # print(f"loop took {end - start} secs")
+        # print("-"*80)
         sample.append(model)
-
+    print("*"*10,f"loop took {time.time() - t} seconds", "*"*10)
     return sample
 
 def parse(X, Y, q):
@@ -89,19 +73,25 @@ def parse(X, Y, q):
     cols = np.append(xi, 'y')
 
     X = np.hstack((X, Y[:, np.newaxis]))
-    
+
+    t = time.time()
     df = pd.DataFrame(X, columns=cols)
-    df = df.assign(count=-1).groupby(cols.tolist()).count().reset_index()
-   
-    allY = pd.DataFrame(np.arange(q), columns=['y'])
 
-    x = []
-    for name, group in df.groupby(xi.tolist()):
-        vx = group.loc[:, ['y', 'count']]
-        vx = pd.merge(vx, allY, how='right', on='y')
-        vx.fillna(0, inplace=True) # replaces NaN with 0
-        vx = vx.sort_values(by=['y']).values
-        x.append(vx)
+    # add a count column containing vx(j)
+    df = df.assign(count=-1).groupby(cols.tolist()).count().reset_index()    
+    g = df.groupby(xi.tolist()).cumcount()
+    # remove 'y' column
+    df = df.loc[:, df.columns != 'y']
 
-    return np.array(x)[...,1]
+    # x is the array [vx1, vx2, ...] where vxi is qxi ndarray for state xi
+    # vxi = <vxi(1), vxi(2), ..., vxi(q-1)>
+    xi = xi.tolist()
+    xi.append(g)
+    x = df.set_index(xi)
+    x = x.unstack(fill_value=0).values
+    
+    if x.shape[1] < q:
+        x_stuffing = np.zeros((x.shape[0], q - x.shape[1]))
+        x = np.hstack((x, x_stuffing))
 
+    return x
